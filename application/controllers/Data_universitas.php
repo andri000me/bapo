f053a24b6e263f7db6568881b0f3950f132d36ba
A Fakhrurozi S <haifahrul@gmail.com>
2019-12-08 22:26:59 +0700

    add menu prodi

--- application/controllers/Data_universitas.php
+++ application/controllers/Data_universitas.php
@@ -14,21 +14,37 @@ class Data_universitas extends MY_Controller
         $this->checkRole();
     }
 
-    // Grocery CRUD - Data Universitas
     public function index()
     {
-        $crud = $this->generate_crud('mst_fakultas', 'Data Universitas');
-        // $crud->display_as('kd_fakulas', 'Kode Fakultas');
-        // $crud->display_as('nama_fakultas', 'Nama Fakultas');
+        $this->render('data_universitas/index', 'with_breadcrumb');
+    }
+
+    // Grocery CRUD - Data Fakultas
+    public function fakultas()
+    {
+        $crud = $this->generate_crud('mst_fakultas', 'Data Fakultas');
+        $crud->display_as('kd_fakultas', 'Kode Fakultas');
+        $crud->display_as('nama_fakultas', 'Nama Fakultas');
         $crud->unset_export();
 
-        $this->mPageTitle = 'Data Universitas';
+        $this->mPageTitle = 'Data Fakultas';
         $this->render_crud();
     }
 
-    // public function index()
-    // {
-    //     // $this->set_js('assets/dist/frontend/app/login.js');
-    // 	$this->render('signin', 'with_breadcrumb');
-    // }
+    public function program_studi()
+    {
+        $crud = $this->generate_crud('mst_program_studi', 'Data Program Studi');
+        $crud->columns('kd_fakultas', 'kd_prodi', 'nama_prodi');
+        $crud->fields('kd_fakultas', 'kd_prodi', 'nama_prodi');
+        $crud->set_relation('kd_fakultas', 'mst_fakultas', '{nama_fakultas}');
+
+        $crud->display_as('kd_fakultas', 'Nama Fakultas');
+        $crud->display_as('nama_fakultas', 'Nama Fakultas');
+        $crud->display_as('kd_prodi', 'Kode Program Studi');
+        $crud->display_as('nama_prodi', 'Nama Program Studi');
+        $crud->unset_export();
+
+        $this->mPageTitle = 'Data Program Studi';
+        $this->render_crud();
+    }
 }
---
+++ application/views/data_universitas/index.php
@@ -0,0 +1,46 @@
+<div class="services">
+    <div class="container">
+        <h3>Data Universitas</h3>
+        <hr>
+        <div class="col-md-6">
+            <img src="<?= base_url() ?>assets/images/yarsi.jpg" class="img-responsive">
+            <p>
+                Universitas YARSI ialah perguruan tinggi Islam swasta yang berada di Cempaka Putih, Jakarta Pusat. Kampus Universitas YARSI berada di pusat kota Jakarta dengan luas lahan kampus 25.000 m² dan luas bangunan 19.300 m² yang terdiri dari berbagai bangunan. Selain itu YARSI telah mempunyai kampus baru Universitas YARSI bertingkat 12 lantai. Saat ini sedang dibangun Rumah Sakit Gigi dan Mulut Pendidikan Universitas YARSI dan Rumah Sakit Pendidikan Universitas YARSI dengan penambahan luas bangunan menjadi 185.000 m2.
+            </p>
+        </div>
+
+        <div class="col-md-6">
+            <div class="media">
+                <ul>
+                    <li>
+                        <div class="media-left">
+                            <i class="fa fa-book"></i>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Data Fakultas</h4>
+                            <a href="data_universitas/fakultas">
+                                <button class="btn btn-primary btn-lg">Lihat Daftar Fakultas</button>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <hr>
+            <div class="media">
+                <ul>
+                    <li>
+                        <div class="media-left">
+                            <i class="fa fa-book"></i>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Data Program Studi</h4>
+                            <a href="data_universitas/program_studi">
+                                <button class="btn btn-primary btn-lg">Lihat Daftar Program Studi</button>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
